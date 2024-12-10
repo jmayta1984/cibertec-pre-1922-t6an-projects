ba5be35fed377faa65e5cbba51c71569cfb9d14b
jmayta1984 <jmayta@pucp.edu.pe>
2024-12-10 08:20:50 -0500

    Add .gitignore and remove unnecessary files

---
+++ session14/EazyShoes/EazyShoes/EazyShoesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EazyShoesApp.swift
+//  EazyShoes
+//
+//  Created by DAMII on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct EazyShoesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
